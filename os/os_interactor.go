1137ab3ee281ce99bfd2c59e90c372809fed9dda
Travis Schoenberg <schotra@chrobinson.com>
2020-08-06 11:48:05 -0500

    Fix run command for Windows

--- os/os_interactor.go
+++ os/os_interactor.go
@@ -1,11 +1,14 @@
 package os
 
 import (
+	"fmt"
 	"io"
+	"io/ioutil"
 	"os"
 	"os/exec"
 	"os/user"
 	"runtime"
+	"strings"
 )
 
 // DefaultInteractor is the default OS interactor that wraps go standard os
@@ -19,14 +22,28 @@ func (i *DefaultInteractor) OpenURL(url string) error {
 
 	switch runtime.GOOS {
 	case "windows":
+		// The character & is treated as running a separate command in Windows
+		// cmd /c start "http://domain.com?param1&param2" results in trying to run cmd /c "start http://domain.com?parm1" & param2
+		// Also, the " char is used as the delimiter to escape special characters, so "&" would become \&\
+		// cmd /c start 'http://domain.com?param1"&"param2' works when inputting directly to the command prompt,
+		// but the "&" is escaped by \"&\" when passed from code, which becomes \&\, resulting in cmd /c start 'http://domain.com?param1\&\param2'
+		// And spaces will not work within the string we need to encode only the whitespace.
+		// SO, this code creates the correct URL string, adds the start command to a powershell file and executes powershell
+		// to allow the browser to be opened for auth.
+		tmpfile := []byte(fmt.Sprintf("start '%s'", strings.ReplaceAll(strings.ReplaceAll(url, " ", "%20"), "&", `"&"`)))
+		if err := ioutil.WriteFile("browser-signin.ps1", tmpfile, 0644); err != nil {
+			return err
+		}
 		cmd = "cmd"
-		args = []string{"/c", "start"}
+		args = []string{"/c", "Powershell", ".\\browser-signin.ps1"}
 	case "darwin":
 		cmd = "open"
+		args = append(args, url)
 	default: // "linux", "freebsd", "openbsd", "netbsd"
 		cmd = "xdg-open"
+		args = append(args, url)
 	}
-	args = append(args, url)
+
 	return exec.Command(cmd, args...).Start()
 }
 
